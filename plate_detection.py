8c0af1a0d0365d5aba61e83813d73eb126bb4562
Hung Le <lehung@grinnell.edu>
2021-04-26 10:17:38 +0700

    Add files via upload

---
+++ plate_detection.py
@@ -0,0 +1,35 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="cloud_api_key.json"
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    snapshot_dir = "snapshots"
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+#detect_text('snapshots/Q2GV-9GBH-4PC3.jpg')
+detect_text('snapshots/test3.jfif')
